55e9c537956e97b35104b7a32f44afcacf75a9a7
yu-ka nishime <e195745@ie.u-ryukyu.ac.jp>
2019-12-29 22:05:01 +0900

    third

--- src/jp/ac/uryukyu/ie/e195745/Main.java
+++ src/jp/ac/uryukyu/ie/e195745/Main.java
@@ -1,18 +1,21 @@
 package jp.ac.uryukyu.ie.e195745;
 
 public class Main {
+	 public static void main(String[] args) {
+	/*
 	@SuppressWarnings("null")
-	public static void main(String[] args) {
 
-		String str = null;
-        try {
-            System.out.println(str.length());
-        }
-        catch (NullPointerException e){
-            System.out.println("NullPointerException発生");
-            System.out.println(e.getMessage());
-        }
-
-	}
-
+    String str = null;
+    try {
+        System.out.println(str.length());
+    }
+    catch (NullPointerException e){
+        System.out.println("NullPointerException発生");
+        System.out.println(e.getMessage());
+    }
+}
+ */
+    String str = "百二十三";
+	int value = Integer.parseInt(str);
+	 }
 }
