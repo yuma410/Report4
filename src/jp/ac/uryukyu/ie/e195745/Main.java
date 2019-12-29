3139b66ede512cb9c0bf47ef950a3f5107e064b4
yu-ka nishime <e195745@ie.u-ryukyu.ac.jp>
2019-12-29 19:05:41 +0900

    first

---
+++ src/jp/ac/uryukyu/ie/e195745/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e195745;
+
+public class Main {
+	public static void main(String[] args) {
+
+		String str = null;
+        try {
+            System.out.println(str.length());
+        }
+
+	}
+
+}
