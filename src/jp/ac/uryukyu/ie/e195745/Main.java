9e419dfc599623a10335835e8f85487519e82764
yu-ka nishime <e195745@ie.u-ryukyu.ac.jp>
2019-12-29 21:50:47 +0900

    second

--- src/jp/ac/uryukyu/ie/e195745/Main.java
+++ src/jp/ac/uryukyu/ie/e195745/Main.java
@@ -1,11 +1,16 @@
 package jp.ac.uryukyu.ie.e195745;
 
 public class Main {
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
 
 		String str = null;
         try {
             System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointerException発生");
+            System.out.println(e.getMessage());
         }
 
 	}
